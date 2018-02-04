2bf1cf0a2195527d5301352fed5e6bf55da5081c
Bo Zhang <bo@gradle.com>
2018-02-04 16:49:13 +0800

    Normalize output when test case name contains parentheses

--- subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -38,7 +38,7 @@ abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
     abstract String getDistributionLabel()
 
     int getLibJarsCount() {
-        195
+        198
     }
 
     def "no duplicate entries"() {
